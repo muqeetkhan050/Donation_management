fa8d1134c4a846fb13c276d4fce22ace1b619727
muqeetkhan <muqeetkhan050@gmail.com>
2025-08-14 23:56:14 +1000

    login failed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,10 +13,9 @@ const registerUser = async (req, res) => {
         const userExists = await User.findOne({ email });
         if (userExists) return res.status(400).json({ message: 'User already exists' });
         
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
 
-        const user = await User.create({ name, email, password: hashedPassword });
+
+        const user = await User.create({ name, email, password});
         res.status(201).json({ id: user.id, name: user.name, email: user.email, token: generateToken(user.id) });
     } catch (error) {
         res.status(500).json({ message: error.message });
