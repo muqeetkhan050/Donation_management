ece19147cd4c0d2bd55390806902f45b360969ae
muqeetkhan <muqeetkhan050@gmail.com>
2025-08-14 23:26:01 +1000

    hashed pas

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,8 +12,11 @@ const registerUser = async (req, res) => {
     try {
         const userExists = await User.findOne({ email });
         if (userExists) return res.status(400).json({ message: 'User already exists' });
+        
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-        const user = await User.create({ name, email, password });
+        const user = await User.create({ name, email, password: hashedPassword });
         res.status(201).json({ id: user.id, name: user.name, email: user.email, token: generateToken(user.id) });
     } catch (error) {
         res.status(500).json({ message: error.message });
