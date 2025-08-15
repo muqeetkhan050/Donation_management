fcc8854662917694792f9307ffc24a114074d473
muqeetkhan <muqeetkhan050@gmail.com>
2025-08-15 22:54:41 +1000

    ODT-22 implemented backend API

--- backend/controllers/donationController.js
+++ backend/controllers/donationController.js
@@ -16,4 +16,23 @@ const addDonation=async (req,res)=>{
 
 }
 
-module.exports = { addDonation };
+const deleteDonation = async (req, res) => {
+  try {
+    const donation = await Donation.findById(req.params.id);
+
+    if (!donation) {
+      return res.status(404).json({ message: 'Donation not found' });
+    }
+
+    if (donation.createdBy.toString() !== req.user.id) {
+      return res.status(401).json({ message: 'Not authorized to delete this donation' });
+    }
+
+    await donation.deleteOne();
+    res.json({ message: 'Donation removed' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { addDonation,deleteDonation };
